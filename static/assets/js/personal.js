73056172254f493c7981d962770c24cd190b1571
ycimadevila <58696039+ycimadevila@users.noreply.github.com>
2022-11-30 11:30:41 -0500

    --

---
+++ static/assets/js/personal.js
@@ -0,0 +1,6 @@
+function mostrar_elemento(id1, id2, value) {
+    var isChecked = document.getElementById(id1).checked;
+    if (isChecked){
+        document.getElementById(id2).append(<i></i>)
+    }
+}
