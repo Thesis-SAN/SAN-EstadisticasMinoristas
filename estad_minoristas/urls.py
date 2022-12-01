9894258dbab73634f19d51fd411c8be82e1c706a
JessyGigato <jessygigato@gmail.com>
2022-12-01 12:37:19 -0800

    inicio creacion diccionarios, utils, consultas con solo una grupacion funcionando, sin el total por la agrupacion

--- estad_minoristas/urls.py
+++ estad_minoristas/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from estad_minoristas.views import index, test, test_table
+from estad_minoristas.views import index, report, test_table
 from login.views import san_login_view
 
 urlpatterns = [
     path('', index),
     path("login/", san_login_view,name = 'login'),
-    path("test/", test, name = 'test'),
+    path("test/", report, name = 'report'),
     path("table/", test_table, name = 'test_table'),
 ]
